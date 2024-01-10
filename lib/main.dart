838d31e9cc257a8c12187d2036d81fe6ab532726
marxsrocha <marxrocha.s@gmail.com>
2024-01-10 11:16:16 -0300

    App 1.0

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/pages/myApp.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/repositories/todo_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_list/models/todo.dart';
+
+const todoListKey = 'todo_list';
+
+class TodoRepository {
+  late SharedPreferences sharedPreferences;
+
+  void saveTodoList(List<Todo> todos) {
+    final jsonString = json.encode(todos); // converte em texto
+    sharedPreferences.setString(todoListKey, jsonString);
+  }
+
+  Future<List<Todo>> getTodoList() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final String jsonString = sharedPreferences.getString(todoListKey) ?? '[]';
+    final List jsonDecoded = json.decode(jsonString) as List;
+    return jsonDecoded.map((e) => Todo.fromJson(e)).toList();
+  }
+
+  //void exemplo() {
+  // sharedPreferences.setString('nome', 'marx');
+  // sharedPreferences.getString('nome');
+  // }
+}
